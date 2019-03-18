91b849aab4fc092a818ec965e37e4f0369f9e3d1
Abhishek Singh <47527051+abhishek042@users.noreply.github.com>
2019-03-19 02:00:42 +0530

    Folder For PY Programs
    i dont think thi will make a folder on github but lets see

---
+++ PY Programs idk/conversoinofdatatypetoprint.py
@@ -0,0 +1,47 @@
+#what ww are doing is adding a idk
+print(float('4.3')-45)
+a=12
+a=+a
+print(a)
+1+2
+print(4*40)
+vard='hiiiiiiiiiiiiiiiiiiiiiii'
+#varibale can include function
+variable=print(vard)
+variable
+#below thig is called unpacking
+x,y=(3,5)
+#unpoacks py list of favrible into x and 5
+print(x)
+print(y)
+#must unpack same no of varibel
+
+#whileloof
+condition =1
+while condition < 10:
+    print(condition)
+    condition +=1
+    #this adda 1 to condition same as condition = conditoin+1
+    
+#while true makes infinaye loop
+#meh its a boolen boi
+#adding a # to stop infinite loop                    
+#while True:
+    print('doing stuff')
+#for loop
+#used for uncertain time frame and stuff like that
+#dammit use whitch you like    
+#irritating through a list
+    
+examplelist =[1,2,3,35,6,346,456,6,4564,6,4,2,3432]
+for eachnumber in examplelist:
+    print(eachnumber)
+#can also use like this
+for i in examplelist:
+    print(i)
+#noww range function built in 
+#range(1,100) cycles thorugh num from 1 to 99 and can be usedd in for loof
+    
+for eachnumber in range(1,100):
+    print(eachnumber)
+    
---
+++ PY Programs idk/new.py
@@ -0,0 +1,4 @@
+print('hi',str(5))
+a=1.30;
+print(a)
+
