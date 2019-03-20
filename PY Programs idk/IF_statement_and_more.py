603044ae191e219e9fd12f0c593f30b118dfc645
Abhishek Singh <47527051+abhishek042@users.noreply.github.com>
2019-03-20 16:43:17 +0530

    Day_2

---
+++ PY Programs idk/IF_statement_and_more.py
@@ -0,0 +1,23 @@
+x=5
+y=8
+if x<y:
+    print('x is greater')
+a=1
+b=7
+c=3
+#this means b is greater than c and a
+if c < b > a:
+    print('somthing')
+
+
+v=1
+f=2
+g=3
+#read as if g is greater tha v AND g is less than f
+#first conditio is true but second is false id if does not execute
+if v < g < f:
+    print('dometjo')
+#you can add n parameters for "if" as you like
+#this shit might get's hairy af
+    
+
